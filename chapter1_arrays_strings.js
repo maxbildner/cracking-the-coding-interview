59a67f5e21c5b2a75b7093f0bc066d8b1a11ae6a
mbildner <mbildner@icloud.com>
2019-12-24 14:34:35 +0800

    Upload 1.3 urlify

--- chapter1_arrays_strings.js
+++ chapter1_arrays_strings.js
@@ -169,5 +169,18 @@ function quickSort(str) {
 
 
 
+
 // *****************************************************************************
-// 1.3 
+// 1.3 URLify: Write a method to replace all spaces in a string with '%20'
+// Assume that the string has sufficient space at the end to hold the additional
+// characters, and that you are given the "true" length of  the string
+// Ex.
+// "Mr John Smith" => "Mr%20John%20Smith"
+// "Mr John Smith" => "Mr%20John%20Smith"
+
+// function urlify(string) {
+//   let words = string.split(" ");
+//   return words.join("%20");
+// }
+
+// console.log(urlify("Mr John Smith"));   // "Mr%20John%20Smith"
