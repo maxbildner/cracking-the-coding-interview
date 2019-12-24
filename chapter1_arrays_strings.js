180cdb3445e13d27fd8567768e987316b9cd2cd0
mbildner <mbildner@icloud.com>
2019-12-24 14:06:23 +0800

    Upload 1.2 solution using sorting technique

--- chapter1_arrays_strings.js
+++ chapter1_arrays_strings.js
@@ -45,6 +45,7 @@ function isUnique(string) {
 
 
 
+
 // *****************************************************************************
 // 1.2 Check Permutation
 // Given two strings, write a method to decide if one is a permutation of the 
@@ -88,34 +89,78 @@ function checkPermutation(str1, str2) {
 
 
 // V2- more efficient, uses hash table for lookup
+// time: O(n), where n = string length of inputs (assumes equal length of both)
+/*
 function checkPermutation(str1, str2) {
   if (str1.length != str2.length) return false;
 
   let str2Hash = {};
   
-  // build hash counter
+  // build hash counter: O(n) time
   for (let i = 0; i < str2.length; i++) {
     let char = str2[i];
+
+    // if char is not in hash, make corresponding value = 1
     if (str2Hash[char] == undefined) {
       str2Hash[char] = 1;
-    } else {
-      str2Hash[char] += 1;
-    }
-  }
-
-  for (let i = 0; i < str1.length; i++) {
-    let char =  str1[i];
-
-    // if char from str1 is in str2
-    if (str2Hash[char]) {
-
-    } else {
+    } else {                  // char is in hash (duplicate) so quit
+      // str2Hash[char] += 1;
       return false;
     }
   }
+
+  return true;
+}
+*/
+
+
+// V3- Sort both inputs first, then compare values
+// Note* assumes inputs all lowercase/no spaces/non alphabetic chars
+/*
+function checkPermutation(str1, str2) {
+  if (str1.length != str2.length) return false;
+  let str1Copy = quickSort(str1);
+  let str2Copy = quickSort(str2);
+
+  return str1Copy === str2Copy;
 }
 
-console.log(checkPermutation("ab", "ba"));
-console.log(checkPermutation("abc", "cba"));
-console.log(checkPermutation("abc", "aba"));
-console.log(checkPermutation("abc", "ab"));
+// Helper funciton sorts alphabetically (lowercase only)
+// returns new sorted string
+// "ecbad" => "abcde"
+function quickSort(str) {
+  // return input str, if input length = 1 (bec sorted already) base case
+  if (str.length <= 1) return str; 
+
+  // choose pivot point (use last char)
+  let pivotIdx = str.length - 1;
+  let pivot = str[pivotIdx];
+  let strCopy = str.slice(0, -1);
+  let left = "";
+  let right = "";
+
+  // loop through each char in strCopy
+  for (let i = 0; i < strCopy.length; i++) {
+    let char = strCopy[i];
+    if (char < pivot) {
+      left = left + char;
+    } else {
+      right = right + char;
+    }
+  }
+
+  return quickSort(left) + pivot + quickSort(right);
+}
+// console.log(quickSort("ecbad"));
+*/
+
+// console.log(checkPermutation("ab", "ba"));      // true
+// console.log(checkPermutation("abc", "cba"));    // true
+// console.log(checkPermutation("abc", "aba"));    // false  
+// console.log(checkPermutation("abc", "ab"));     // false
+
+
+
+
+// *****************************************************************************
+// 1.3 
